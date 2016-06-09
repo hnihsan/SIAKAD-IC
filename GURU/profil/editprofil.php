6dd725b4cf51a89b8bd7e7ffb290af9ad603cedf
hnihsan <helmi.n.ihsan@gmail.com>
2016-06-09 13:05:25 +0700

    Add files via upload

---
+++ GURU/profil/editprofil.php
@@ -0,0 +1,7 @@
+<h3>ubah</h3>
+<form>
+	<p>ID : <input type=text name='idmember' required maxlength='10'></p>
+	<p>Nama : <input type=text name='nmmember' required maxlength='10'></p>	
+	<input type=submit name='ubah' value='ubah'>
+	<a href='index.php?hal=profil'>Kembali</a>
+</form>
---
+++ GURU/profil/profil.php
@@ -0,0 +1,20 @@
+<?php
+//ambil judul dan data member dari database
+?>
+<h2 class='judul'>Profil</h2>
+	<p>
+		<table>			
+			<tr>
+				<th>ID :</th>
+				<th></th>				
+			</tr>
+			<tr>
+				<th>Nama :</th>
+				<th></th>				
+			</tr>
+			<tr>
+				<th>Alamat :</th>
+				<th></th>				
+			</tr>			
+		</table>
+		<p><button>Ubah</button><a href="index.php?hal=home">Kembali</a></p>
