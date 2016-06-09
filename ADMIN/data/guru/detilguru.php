cc8ed1b7b27bb63ae689be28ad577db562a83895
hnihsan <helmi.n.ihsan@gmail.com>
2016-06-09 12:56:50 +0700

    upload file guru

---
+++ ADMIN/data/guru/detilguru.php
@@ -0,0 +1,56 @@
+<h2 class="judul">Data Guru</h2>
+<p>
+	<table>		
+		<tr>
+			<th>NIP :</th>
+			<td>0001</td>
+		</tr>	
+		<tr>	
+			<th>Nama :</th>
+			<td>Guru 1</td>
+		</tr>
+		<tr>	
+			<th>Alamat :</th>
+			<td>Jalan2</td>
+		</tr>
+		<tr>	
+			<th>tempat Lahir :</th>
+			<td>Jakarta</td>
+		</tr>
+		<tr>	
+			<th>Tanggal Lahir :</th>
+			<td>01-01-1990</td>
+		</tr>
+		<tr>
+			<th>Wali Kelas :</th>
+			<td>X1</td>
+		</tr>
+		<tr>	
+			<th>Mata Pelajaran :</th>
+			<td>Pelajaran 1</td>
+		</tr>
+		<tr>	
+			<th>Jadwal Pengajaran :</th>
+			<td>
+				<table border="2">
+					<tr>
+						<th>No.</th>
+						<th>Kode</th>
+						<th>Pelajaran</th>
+						<th>Kelas</th>
+						<th>Sesi</th>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>J01</td>
+						<td>Pelajaran 1</td>
+						<td>X3</td>
+						<td>Senin, 08.00-09.00</td>
+					</tr>
+				</table>
+			</td>
+		</tr>	
+		
+	</table>
+	<a href="index.php?hal=dbguru">Kembali</a>
+</p>
---
+++ ADMIN/data/guru/guru.php
@@ -0,0 +1,23 @@
+<h2 class='judul'>Data Guru Islamic Center</h2>
+<p>
+		<table>
+			<tr>
+				<th>No.</th>
+				<th>NIP</th>
+				<th>Nama</th>				
+				<th>Wali Kelas</th>
+
+			</tr>
+			<tr>
+				<td>1</td>
+				<td>0001</a></td>
+				<td><a href="index.php?hal=dtlguru&q=#">Guru 1</a></td>
+				<td>X3</td>
+			</tr>
+			<tr>
+				<td>+</td>
+				<td><a href="index.php?hal=tmguru">Tambah data..</a></td>
+			</tr>			
+		</table>
+					
+</p>
